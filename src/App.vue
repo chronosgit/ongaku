2810189b0d454801b9dfafa298afbb49b43e15dd
chronosgit <warfacemeister@mail.ru>
2024-09-11 22:48:20 +0500

    improvement: updated App.vue & main.js

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,5 @@
-<template><div id="app">App</div></template>
+<template>
+	<div id="app">
+		<h1>App</h1>
+	</div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
 import App from './App.vue';
 import './style.css';
 
-createApp(App).mount('#app');
+const app = createApp(App);
+const i18n = createI18n({ locale: 'en', fallbackLocale: 'en' });
+
+app.config.errorHandler = (err, vm, info) => {
+	const errorComponentName = vm.$options.__name;
+
+	console.error(
+		`${err}\nFailed component: ${errorComponentName}\nOccured in ${info}`
+	);
+};
+
+app.use(i18n).mount('#app');
