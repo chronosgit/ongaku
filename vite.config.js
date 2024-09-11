897c0cd831226bc5614f120ab87d1a955241cb54
chronosgit <warfacemeister@mail.ru>
2024-09-11 22:47:49 +0500

    feat: added path aliasing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@assets': path.resolve(__dirname, './src/assets'),
+			'@common': path.resolve(__dirname, './src/common'),
+			'@locales': path.resolve(__dirname, './src/locales'),
+			'@modules': path.resolve(__dirname, './src/modules'),
+			'@router': path.resolve(__dirname, './src/router'),
+			'@services': path.resolve(__dirname, './src/services'),
+			'@store': path.resolve(__dirname, './src/store'),
+		},
+	},
 });
