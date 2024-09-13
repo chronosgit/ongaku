705adfd757b1539ea3b154ce076c69f116459ead
chronosgit <warfacemeister@mail.ru>
2024-09-13 18:37:42 +0500

    improvement: updated fallback locale for i18n

--- src/localization/i18n.js
+++ src/localization/i18n.js
@@ -1,46 +1,40 @@
-import { nextTick } from 'vue';
+import { nextTick, ref } from 'vue';
 import { createI18n } from 'vue-i18n';
 
-export const SUPPORTED_LOCALES = ['en', 'ru'];
-export const DEFAULT_LOCALE = 'en';
+const SUPPORTED_LOCALES = ['en', 'ru'];
+const DEFAULT_LOCALE = 'en';
 
-export const setupI18n = (initialLocale) => {
-	const validInitLocale = SUPPORTED_LOCALES.includes(initialLocale)
-		? initialLocale
-		: 'en';
+const setupI18n = (possibleInitLocale) => {
+	const validInitLocale = SUPPORTED_LOCALES.includes(possibleInitLocale)
+		? possibleInitLocale
+		: DEFAULT_LOCALE;
 
 	const options = {
 		locale: validInitLocale,
-		fallbackLocale: 'en',
+		fallbackLocale: validInitLocale,
 		legacy: false,
+		messages: {},
 	};
 
 	const i18n = createI18n(options);
 
-	setI18nLanguage(i18n, options.locale);
-
-	// initial load
-	loadLocaleMessages(i18n, options.locale);
-
 	return i18n;
 };
 
-export const setI18nLanguage = (i18n, locale) => {
-	if (i18n.mode === 'legacy') {
-		i18n.global.locale = locale;
-	} else {
+// updates language (dependent on redirection, because it's called in trackLocaleBeforeEach)
+const loadLocaleMessages = async (i18n, locale) => {
+	try {
+		const messages = await import(`./locales/${locale}.json`);
+
+		// axios.defaults.headers.common['Accept-Language'] = locale
+		document.querySelector('html').setAttribute('lang', locale);
+		i18n.global.setLocaleMessage(locale, messages.default);
 		i18n.global.locale.value = locale;
+
+		return nextTick();
+	} catch (error) {
+		console.error(`Failed to load messages for locale: ${locale}`, error);
 	}
-
-	// axios.defaults.headers.common['Accept-Language'] = locale
-	document.querySelector('html').setAttribute('lang', locale);
 };
 
-export const loadLocaleMessages = async (i18n, locale) => {
-	let messages = await import(`./locales/${locale}.json`);
-
-	// set locale and locale message
-	i18n.global.setLocaleMessage(locale, messages.default);
-
-	return nextTick();
-};
+export { SUPPORTED_LOCALES, DEFAULT_LOCALE, setupI18n, loadLocaleMessages };
