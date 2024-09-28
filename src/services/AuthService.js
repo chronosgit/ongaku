f39358c5d82503034bb86f1f33a67cd0e3755f81
chronosgit <warfacemeister@mail.ru>
2024-09-28 16:07:03 +0500

    improvement: AuthService now requires more perms

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -62,6 +62,8 @@ class AuthService {
 					'user-read-email',
 					'user-library-read',
 					'user-library-modify',
+					'playlist-modify-public',
+					'playlist-modify-private',
 				];
 				const scope = scopes.join(' ');
 				const clientId = import.meta.env?.VITE_SPOTIFY_CLIENT_ID;
