382a9f767dec68ec7f6420907581897d4dfb59f4
chronosgit <warfacemeister@mail.ru>
2024-10-01 21:44:08 +0500

    ✨ feat: added nuxt-i18n
    Added i18n module with configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,6 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
 	devtools: { enabled: true },
-
 	components: false,
 
 	alias: {
@@ -15,5 +14,11 @@ export default defineNuxtConfig({
 			tailwindcss: {},
 			autoprefixer: {},
 		},
+	},
+
+	modules: ['@nuxtjs/i18n'],
+
+	i18n: {
+		vueI18n: './i18n.config.ts', // if you are using custom path, default
 	},
 });
