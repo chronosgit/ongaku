e24b1989e2f96ec3731ab444a1cd167f5b869455
chronosgit <warfacemeister@mail.ru>
2024-10-02 15:41:23 +0500

    🚧 wip: started doing spotify auth redir. component
    Started implementing migrated SpotifyAuthRedirect component

---
+++ components/SpotifyAuthRedirect.vue
@@ -0,0 +1,18 @@
+<template>
+	<div>Redirecting...</div>
+</template>
+
+<script>
+	export default {
+		mounted() {
+			const urlParams = new URLSearchParams(window.location.search);
+			const code = urlParams.get('code');
+
+			if (code) {
+				window.opener.postMessage({ code }, window.location.origin);
+
+				window.close();
+			}
+		},
+	};
+</script>
