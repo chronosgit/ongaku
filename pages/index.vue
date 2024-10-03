90d7004e1def09f28b40da322c75ab3514e7e5d6
chronosgit <warfacemeister@mail.ru>
2024-10-03 14:47:07 +0500

    🚧 wip: further changes in the home page
    Testing services

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,22 @@
 <script setup>
+	import AuthService from '~/services/AuthService';
+
 	definePageMeta({
 		title: 'home.meta.title',
 	});
 
-	const { data, error, execute } = useLazyFetch('/api/healthcheck');
+	const { data, error, execute } = useLazyAsyncData('oauth-request-uri', () => {
+		return AuthService.requestOAuthUri();
+	});
 
 	watch(data, () => console.log(data.value));
-	watch(error, () => console.error(error));
+	watch(error, () => console.error(error.value));
 </script>
 
 <template>
-	<div class="">Home</div>
+	<div class="mb-3">Home</div>
 
-	<button @click="execute()">Execute</button>
+	<button class="px-4 py-2 border-[1px] rounded-lg" @click="execute()">
+		Request OAuth URI
+	</button>
 </template>
