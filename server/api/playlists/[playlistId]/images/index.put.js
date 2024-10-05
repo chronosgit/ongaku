83a0fc6fe443f0f341a8c1e36a42d9c7b10df534
chronosgit <warfacemeister@mail.ru>
2024-10-05 17:32:26 +0500

    ✨ feat: added /api/playlists/:id/images PUT endpoint
    Users can update a playlist's cover image by using its ID

---
+++ server/api/playlists/[playlistId]/images/index.put.js
@@ -0,0 +1,19 @@
+// https://developer.spotify.com/documentation/web-api/reference/upload-custom-playlist-cover
+export default defineEventHandler(async (e) => {
+	try {
+		const $spotify = e.context.spotify;
+		const playlistId = getRouterParam(e, 'playlistId');
+		const base64EncodedJPEGImage = await readBody(e);
+
+		const res = await $spotify(`/playlists/${playlistId}/images`, {
+			method: 'PUT',
+			body: base64EncodedJPEGImage,
+		});
+
+		return getSuccessResponse(200, 'Image updated', res);
+	} catch (err) {
+		console.error(err);
+
+		throw createError(getErrorOptions(err));
+	}
+});
