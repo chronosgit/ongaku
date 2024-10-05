face7a583d4ae0d78381792dfcd6f57bf0729701
chronosgit <warfacemeister@mail.ru>
2024-10-05 23:07:55 +0500

    ✨ added /api/me/player/devices GET endpoint
    Users can get a list of available player devices

---
+++ server/api/me/player/devices.get.js
@@ -0,0 +1,14 @@
+// https://developer.spotify.com/documentation/web-api/reference/get-a-users-available-devices
+export default defineEventHandler(async (e) => {
+	try {
+		const $spotify = e.context.spotify;
+
+		const res = await $spotify('/me/player/devices');
+
+		return getSuccessResponse(200, 'Devices received', res);
+	} catch (err) {
+		console.error(err);
+
+		throw createError(getErrorOptions(err));
+	}
+});
