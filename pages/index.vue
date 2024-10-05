93affdeaa1ebe7053c42056d07c652bdfbe1eee0
chronosgit <warfacemeister@mail.ru>
2024-10-05 17:05:00 +0500

    ✨ feat: added /api/browse/featured-playlists GET endpoint
    Users can get a list of Spotify featured playlists

--- pages/index.vue
+++ pages/index.vue
@@ -8,15 +8,7 @@
 	const { data, error, execute } = useLazyAsyncData(
 		'fetch',
 		() =>
-			$fetch('/api/users/317adpnoxtrdgo45hirq7dztto3q/playlists', {
-				method: 'POST',
-				body: {
-					name: 'DevPlaylist',
-					public: false,
-					collaborative: false,
-					description: 'Descr be L',
-				},
-			}),
+			$fetch('/api/browse/featured-playlists', { params: { locale: 'ru_RU' } }),
 		{ immediate: false }
 	);
 
