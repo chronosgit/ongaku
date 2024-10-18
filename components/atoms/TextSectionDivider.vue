ac10219680424121a1fd431988bb5313e4595544
chronosgit <warfacemeister@mail.ru>
2024-10-19 01:56:31 +0500

    ✨ feat: added TextSectionDivider atom

---
+++ components/atoms/TextSectionDivider.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+	const props = withDefaults(
+		defineProps<{
+			lineClass?: string; // tw border
+			textClass?: string; // tw
+		}>(),
+		{
+			lineClass: 'border-black',
+			textClass: ' text-black',
+		}
+	);
+</script>
+
+<template>
+	<div class="flex items-center">
+		<div class="flex-grow border-t" :class="props.lineClass"></div>
+
+		<span class="mx-2 text-xs uppercase" :class="props.textClass">
+			<slot></slot>
+		</span>
+
+		<div class="flex-grow border-t" :class="props.lineClass"></div>
+	</div>
+</template>
