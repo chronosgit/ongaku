7824a57b9e2d3395aa67f4df1af5c397e3f3c3f4
chronosgit <warfacemeister@mail.ru>
2024-10-18 23:41:53 +0500

    ✨ feat: added the 404 page

--- pages/not-found.vue
+++ pages/not-found.vue
@@ -1,1 +1,30 @@
-<template>Not Found</template>
+<script setup lang="ts">
+	import OngakuLogo from '~/components/atoms/OngakuLogo.vue';
+
+	definePageMeta({
+		title: '/not-found.meta.title',
+	});
+</script>
+
+<template>
+	<main
+		class="flex min-h-screen flex-col items-center justify-center gap-8 bg-white dark:bg-[#121212]"
+	>
+		<OngakuLogo sizes="70" />
+
+		<h1 class="text-center text-4xl font-bold sm:text-5xl dark:text-white">
+			{{ $t('/not-found.feedback.title') }}
+		</h1>
+
+		<p class="dark:text-[#a7a7a7]">
+			{{ $t('/not-found.feedback.message') }}
+		</p>
+
+		<NuxtLink
+			to="/"
+			class="rounded-full bg-black px-7 py-3 font-bold text-white dark:bg-white dark:text-black"
+		>
+			{{ $t('/not-found.buttons.main') }}
+		</NuxtLink>
+	</main>
+</template>
