56d5fff5959330cbc4e5c4a25f00debc14bc55eb
chronosgit <warfacemeister@mail.ru>
2024-10-19 01:30:44 +0500

    ✨ feat: updated the useClickaway client composable

---
+++ composables/useClickaway.client.ts
@@ -0,0 +1,31 @@
+export default function (templateRefKey: string) {
+	const templateRef = useTemplateRef(templateRefKey) as Ref<
+		HTMLElement | ComponentPublicInstance | null
+	>;
+
+	const { value: isActive, activate, disactivate, toggle } = useToggle();
+
+	const onDocumentClick = (e: MouseEvent) => {
+		const clickedTarget = e.target as Element;
+
+		if (!clickedTarget || !templateRef?.value) return;
+
+		let checkedEl: HTMLElement | null = null;
+
+		if (templateRef.value instanceof HTMLElement) {
+			checkedEl = templateRef.value;
+		} else {
+			checkedEl = templateRef.value.$el;
+		}
+
+		if (checkedEl && !checkedEl.contains(clickedTarget)) {
+			isActive.value = false;
+		}
+	};
+
+	onMounted(() => document.addEventListener('mousedown', onDocumentClick));
+
+	onUnmounted(() => document.removeEventListener('mousedown', onDocumentClick));
+
+	return { isActive, activate, disactivate, toggle };
+}
