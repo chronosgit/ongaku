8b9d390a3c1636c845b4c86ead5168aef7ecc39f
chronosgit <warfacemeister@mail.ru>
2024-10-18 22:01:32 +0500

    🗑️ remove: removed some code from /home index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,9 @@
 <script setup>
-	import AuthService from '~/services/AuthService';
-
 	definePageMeta({
 		title: 'home.meta.title',
 	});
-
-	const { data, error, execute } = useLazyAsyncData(
-		'fetch',
-		() => $fetch('/api/me/player/play', { method: 'PUT' }),
-		{ immediate: false }
-	);
-
-	// "b4f737b9427c9a9a6c21195dd09fa445d52b4430"
-
-	watch(data, () => console.log(data?.value));
-	watch(error, () => console.error(error?.value));
 </script>
 
 <template>
 	<div class="mb-3">Home</div>
-
-	<div class="my-4">
-		<button
-			class="p-2 border-[1px] rounded-lg"
-			@click="AuthService.initOAuth()"
-		>
-			Request OAuth URI
-		</button>
-
-		<button
-			class="p-2 border-[1px] rounded-lg"
-			@click="AuthService.refreshTokens()"
-		>
-			Refresh tokens
-		</button>
-
-		<button class="p-2 border-[1px] rounded-lg" @click="execute()">
-			Use service
-		</button>
-	</div>
 </template>
