557ca7c117f0bfa578af6afb20b2d102e1aa279e
chronosgit <warfacemeister@mail.ru>
2024-10-18 23:36:55 +0500

    🔧 config: updated the nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,10 +34,10 @@ export default defineNuxtConfig({
 	image: {
 		dir: 'assets/images',
 		screens: {
-			sm: 640,
-			md: 768,
-			lg: 1024,
-			xl: 1280,
+			'sm': 640,
+			'md': 768,
+			'lg': 1024,
+			'xl': 1280,
 			'2xl': 1536,
 		},
 	},
