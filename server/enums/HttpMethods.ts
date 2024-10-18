139dd6b48299e37c8e279f75e9e25572c1696432
chronosgit <warfacemeister@mail.ru>
2024-10-19 04:48:13 +0500

    ✨ feat: added server-side enums

---
+++ server/enums/HttpMethods.ts
@@ -0,0 +1,23 @@
+enum HttpMethods {
+	GET,
+	HEAD,
+	PATCH,
+	POST,
+	PUT,
+	DELETE,
+	CONNECT,
+	OPTIONS,
+	TRACE,
+	get,
+	head,
+	patch,
+	post,
+	put,
+	delete,
+	connect,
+	options,
+	trace,
+	undefined,
+}
+
+export default HttpMethods;
