9c2d352005e4c801021bf614aa4ab36b6f45dd8b
chronosgit <warfacemeister@mail.ru>
2024-10-19 01:30:30 +0500

    ✨ feat: updated useToggle composable

--- composables/useToggle.ts
+++ composables/useToggle.ts
@@ -1,11 +1,11 @@
 export default function (initValue: boolean = false) {
 	const value = ref(initValue);
 
-	const toggleValue = () => (value.value = !value.value);
+	const toggle = () => (value.value = !value.value);
 
-	const activateValue = () => (value.value = true);
+	const activate = () => (value.value = true);
 
-	const disactivateValue = () => (value.value = false);
+	const disactivate = () => (value.value = false);
 
-	return { value, toggleValue, activateValue, disactivateValue };
+	return { value, toggle, activate, disactivate };
 }
