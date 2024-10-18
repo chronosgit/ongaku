2ce7d0a54c894f14b208b5580e0b91793f0196f2
chronosgit <warfacemeister@mail.ru>
2024-10-18 22:47:00 +0500

    🔧 config: updated the nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
 		store: './store',
 	},
 
+	plugins: ['~/plugins/dark-mode.client.ts'],
 	runtimeConfig: {
 		public: {
 			spotifyAuthClientId: import.meta.env?.SPOTIFY_AUTH_CLIENT_ID,
