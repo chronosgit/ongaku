98e66c22a927b266184789575da9fc64a924e63c
chronosgit <warfacemeister@mail.ru>
2024-10-18 21:44:12 +0500

    ✨ feat: migrated utils to TypeScript

--- utils/base64Encode.ts
+++ utils/base64Encode.ts
@@ -1,4 +1,4 @@
-export default function (input) {
+export default function (input: ArrayBuffer) {
 	return btoa(String.fromCharCode(...new Uint8Array(input)))
 		.replace(/=/g, '')
 		.replace(/\+/g, '-')
