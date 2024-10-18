00196e159fb77078ee2513de88de382bb3a4c4af
chronosgit <warfacemeister@mail.ru>
2024-10-19 04:03:02 +0500

    ✨ feat: updated Dropdown molecule

---
+++ components/molecules/Dropdown.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+	const props = defineProps<{ isOpen: boolean }>();
+</script>
+
+<template>
+	<div class="absolute" :class="{ 'bg-blue-400': props.isOpen }">
+		<!-- Content inside the dropdown -->
+		<slot></slot>
+	</div>
+</template>
