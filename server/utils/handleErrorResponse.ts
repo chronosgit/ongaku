cc175f5e898b45845662531a08675e22950a8f74
chronosgit <warfacemeister@mail.ru>
2024-10-19 06:10:39 +0500

    ✨ feat: updated handleErrorResponse server-side util

--- server/utils/handleErrorResponse.ts
+++ server/utils/handleErrorResponse.ts
@@ -1,7 +1,9 @@
 import { FetchError } from 'ofetch';
 
 export default function (err: unknown) {
-	if (err instanceof FetchError) throw createError(getErrorOptions(err));
+	if (err instanceof FetchError) {
+		throw createError(getErrorResponse(err.statusCode || 500));
+	}
 
 	throw err;
 }
