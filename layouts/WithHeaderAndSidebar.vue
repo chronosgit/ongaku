5c2de8b1ff14bf843e92b57772d4331bae3762ac
chronosgit <warfacemeister@mail.ru>
2024-10-20 03:11:23 +0500

    ✨ feat: updated layouts

--- layouts/WithHeaderAndSidebar.vue
+++ layouts/WithHeaderAndSidebar.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 	import Default from '~/layouts/default.vue';
 	import MyHeader from '~/components/organisms/MyHeader.vue';
+	import UnauthenticationGuard from '~/components/organisms/UnauthenticationGuard.vue';
 	import LibrarySidebar from '~/components/organisms/LibrarySidebar.vue';
+	import { useCurrentUserStore } from '~/store/useCurrentUserStore';
+
+	const curUserStore = useCurrentUserStore();
 </script>
 
 <template>
@@ -9,11 +13,17 @@
 		<div class="max-h-screen overflow-hidden">
 			<MyHeader />
 
-			<div class="relative h-screen">
-				<LibrarySidebar />
+			<template v-if="curUserStore.isAuthenticated">
+				<div class="relative h-screen">
+					<LibrarySidebar />
 
-				<slot></slot>
-			</div>
+					<slot></slot>
+				</div>
+			</template>
+
+			<template v-else>
+				<UnauthenticationGuard />
+			</template>
 		</div>
 	</Default>
 </template>
