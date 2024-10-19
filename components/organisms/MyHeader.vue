a0046379d513b2e492872d46d3480291c17f4f04
chronosgit <warfacemeister@mail.ru>
2024-10-20 01:23:01 +0500

    🩹 fix: updated hover styling in Icon inside MyHeader

--- components/organisms/MyHeader.vue
+++ components/organisms/MyHeader.vue
@@ -23,35 +23,35 @@
 	<header class="flex items-center justify-between px-2 py-1 dark:bg-black">
 		<!-- Left side -->
 		<div class="flex items-center gap-3">
-			<IconRoundWrapper class="cursor-pointer">
+			<IconRoundWrapper class="group cursor-pointer">
 				<IconLibrary
 					sizes="40"
-					class="scale-125 text-[#b3b3b3] transition-colors hover:text-white"
+					class="scale-125 text-[#b3b3b3] transition-colors group-hover:text-white"
 				/>
 			</IconRoundWrapper>
 		</div>
 
 		<!-- Middle side -->
 		<div class="flex items-center gap-3">
-			<IconRoundWrapper class="cursor-pointer" @click="onHouseClick()">
+			<IconRoundWrapper class="group cursor-pointer" @click="onHouseClick()">
 				<IconHouse
-					class="scale-150 text-[#b3b3b3] transition-colors hover:text-white"
+					class="scale-150 text-[#b3b3b3] transition-colors group-hover:text-white"
 				/>
 			</IconRoundWrapper>
 
 			<!-- TODO: super-responsive searchbar with logic -->
-			<IconRoundWrapper class="cursor-pointer">
+			<IconRoundWrapper class="group cursor-pointer">
 				<IconMagnifier
-					class="scale-150 text-[#b3b3b3] transition-colors hover:text-white"
+					class="scale-150 text-[#b3b3b3] transition-colors group-hover:text-white"
 				/>
 			</IconRoundWrapper>
 		</div>
 
 		<!-- Right side -->
 		<!-- Mobile -->
-		<IconRoundWrapper class="cursor-pointer md:hidden" @click="open()">
+		<IconRoundWrapper class="group cursor-pointer md:hidden" @click="open()">
 			<IconBurgerMenu
-				class="scale-150 text-[#b3b3b3] transition-colors hover:text-white"
+				class="scale-150 text-[#b3b3b3] transition-colors group-hover:text-white"
 			/>
 		</IconRoundWrapper>
 
