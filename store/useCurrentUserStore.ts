8cf9dfb56b09e094e06ad4a44e647c454224b600
chronosgit <warfacemeister@mail.ru>
2024-10-20 04:42:30 +0500

    ✨ feat: updated useCurrentUserStore store

--- store/useCurrentUserStore.ts
+++ store/useCurrentUserStore.ts
@@ -1,22 +1,31 @@
 import type ICurrentUser from '~/interfaces/ICurrentUser';
 
-export const useCurrentUserStore = defineStore('currentUserStore', () => {
-	const user = ref<ICurrentUser>();
-	const isAuthenticated = ref(false);
+export const useCurrentUserStore = defineStore(
+	'currentUserStore',
+	() => {
+		const user = ref<ICurrentUser>();
+		const isAuthenticated = ref(false);
 
-	const authenticateState = () => (isAuthenticated.value = true);
+		const authenticateState = () => (isAuthenticated.value = true);
 
-	const disauthenticateState = () => (isAuthenticated.value = false);
+		const disauthenticateState = () => (isAuthenticated.value = false);
 
-	const updateUser = (loggedInUser: ICurrentUser) => {
-		console.log(loggedInUser);
-	};
+		const updateUser = (loggedInUser: ICurrentUser) => {
+			console.log(loggedInUser);
+		};
 
-	return {
-		user,
-		isAuthenticated,
-		authenticateState,
-		disauthenticateState,
-		updateUser,
-	};
-});
+		return {
+			user,
+			isAuthenticated,
+			authenticateState,
+			disauthenticateState,
+			updateUser,
+		};
+	},
+	{
+		persist: {
+			storage: piniaPluginPersistedstate.cookies(),
+			pick: ['isAuthenticated'],
+		},
+	}
+);
