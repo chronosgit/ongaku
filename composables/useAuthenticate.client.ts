3ba63255c1d9be810f2d1eead00acf5e106782ae
chronosgit <warfacemeister@mail.ru>
2024-10-19 11:08:19 +0500

    ✨ feat: updated the useAuthenticate client composable

--- composables/useAuthenticate.client.ts
+++ composables/useAuthenticate.client.ts
@@ -1,15 +1,9 @@
 import AuthService from '~/services/AuthService';
 
 export default function () {
-	const { locale } = useI18n();
-
 	const authenticate = async () => {
 		try {
 			await AuthService.initOAuth();
-
-			console.log('Authenticated');
-
-			await navigateTo(`/${locale.value}`);
 		} catch (err) {
 			console.error(err);
 		}
