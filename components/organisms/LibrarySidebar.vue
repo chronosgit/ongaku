749d38d1a7852fff816cc5831baa02eef981fee4
chronosgit <warfacemeister@mail.ru>
2024-10-20 01:42:35 +0500

    🚧 wip: working on LibrarySidebar organism

--- components/organisms/LibrarySidebar.vue
+++ components/organisms/LibrarySidebar.vue
@@ -1,3 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+	import { useLayoutStore } from '~/store/useLayoutStore';
 
-<template>Library</template>
+	const { isLeftSideVisible } = useLayoutStore();
+</script>
+
+<template>
+	<aside
+		class="fixed bottom-0 left-0 top-0 bg-red-400"
+		:class="{
+			'translate-x-0': isLeftSideVisible,
+			'-translate-x-full': !isLeftSideVisible,
+		}"
+	>
+		Sidebar
+	</aside>
+</template>
