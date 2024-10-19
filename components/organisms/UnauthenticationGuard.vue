7ddf1e0c1a577fe0867bc78d49951ce8ca7109e2
chronosgit <warfacemeister@mail.ru>
2024-10-20 03:24:49 +0500

    ✨ feat: updated UnauthenticationGuard organism

--- components/organisms/UnauthenticationGuard.vue
+++ components/organisms/UnauthenticationGuard.vue
@@ -6,16 +6,21 @@
 	<div
 		class="flex h-screen flex-col items-center justify-center gap-4 dark:bg-black"
 	>
-		<h1 class="text-3xl font-bold dark:text-white">Oops!</h1>
+		<h1 class="text-3xl font-bold dark:text-white">
+			{{ $t('modules.unauthentication-guard.oops') }}
+		</h1>
 
 		<p class="text-lg dark:text-gray-400">
-			You have to
+			{{ $t('modules.unauthentication-guard.feedback-p1') }}
+
 			<NuxtLink
 				:to="localePath('/auth')"
 				class="text-blue-500 transition-colors hover:text-blue-700"
-				>authenticate</NuxtLink
 			>
-			in order to continue
+				{{ $t('modules.unauthentication-guard.feedback-p2') }}
+			</NuxtLink>
+
+			{{ $t('modules.unauthentication-guard.feedback-p3') }}
 		</p>
 	</div>
 </template>
