253f856e7f992388afe742043d60ed3a01200281
chronosgit <warfacemeister@mail.ru>
2024-10-19 11:57:11 +0500

    ✨ feat: updated MobileRightMenu organism

--- components/organisms/MobileRightMenu.vue
+++ components/organisms/MobileRightMenu.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
+	import Dropdown from '~/components/molecules/Dropdown.vue';
 	import IconDoorOpen from '~/components/atoms/icons/IconDoorOpen.vue';
 	import IconLocalization from '~/components/atoms/icons/IconLocalization.vue';
 	import IconSun from '~/components/atoms/icons/IconSun.vue';
 	import IconMoon from '~/components/atoms/icons/IconMoon.vue';
-	import Dropdown from '~/components/molecules/Dropdown.vue';
 	import IconArrowUp from '~/components/atoms/icons/IconArrowUp.vue';
 	import IconArrowDown from '~/components/atoms/icons/IconArrowDown.vue';
 	import FlexTextSectionDivider from '~/components/atoms/FlexTextSectionDivider.vue';
+	import IconUser from '~/components/atoms/icons/IconUser.vue';
 	import { useColorModeStore } from '~/store/useColorModeStore';
+	import { useCurrentUserStore } from '~/store/useCurrentUserStore';
 
 	const props = defineProps<{ isOpen: boolean }>();
 
-	const { locale, setLocale } = useI18n();
+	const { setLocale } = useI18n();
+	const localePath = useLocalePath();
+
 	const colorModeStore = useColorModeStore();
+	const { isAuthenticated } = useCurrentUserStore();
 
 	const { isActive: isLocaleDropdown, toggle: toggleLocaleDropdown } =
 		useClickawayClient('localization-ref');
 
-	const onAuthClick = async () => await navigateTo(`/${locale.value}/auth`);
+	const onAuthClick = () => navigateTo(localePath('/auth'));
 </script>
 
 <template>
@@ -25,8 +30,31 @@
 		class="fixed right-0 top-0 z-10 h-screen min-w-56 bg-gradient-to-b from-gray-200 to-blue-100 py-2 transition-transform dark:bg-[#121212] dark:bg-none dark:text-white"
 		:class="{ 'translate-x-0': props.isOpen, 'translate-x-72': !props.isOpen }"
 	>
-		<!-- Authenticated -->
-		<template v-if="!true"></template>
+		<!-- User is authenticated -->
+		<!-- Profile section -->
+		<template v-if="!isAuthenticated">
+			<FlexTextSectionDivider
+				text-class="text-gray-500"
+				line-class="border-gray-500"
+				class="px-2"
+			>
+				{{ $t('modules.mobile-right-menu.dividers.profile') }}
+			</FlexTextSectionDivider>
+
+			<div
+				class="flex cursor-pointer items-center gap-2 p-4 text-gray-400 transition-all hover:scale-105 hover:text-black dark:hover:text-white"
+			>
+				<ClientOnly>
+					<div class="scale-125">
+						<IconUser />
+					</div>
+				</ClientOnly>
+
+				<NuxtLink class="font-bold">
+					{{ $t('modules.mobile-right-menu.buttons.profile') }}
+				</NuxtLink>
+			</div>
+		</template>
 
 		<!-- User is unauthenticated -->
 		<!-- Authentication section -->
@@ -39,20 +67,17 @@
 				{{ $t('modules.mobile-right-menu.dividers.auth') }}
 			</FlexTextSectionDivider>
 
-			<div class="flex flex-col gap-4 px-4 py-4">
-				<!-- Auth option -->
-				<div
-					class="flex cursor-pointer items-center gap-2 text-gray-400 transition-all hover:scale-105 hover:text-black dark:hover:text-white"
-					@click="onAuthClick"
-				>
-					<ClientOnly>
-						<IconDoorOpen class="scale-125" />
-					</ClientOnly>
+			<div
+				class="flex cursor-pointer items-center gap-2 p-4 text-gray-400 transition-all hover:scale-105 hover:text-black dark:hover:text-white"
+				@click="onAuthClick"
+			>
+				<ClientOnly>
+					<IconDoorOpen class="scale-125" />
+				</ClientOnly>
 
-					<button to="/auth" class="font-bold">
-						{{ $t('modules.mobile-right-menu.buttons.auth') }}
-					</button>
-				</div>
+				<button to="/auth" class="font-bold">
+					{{ $t('modules.mobile-right-menu.buttons.auth') }}
+				</button>
 			</div>
 		</template>
 
@@ -90,8 +115,7 @@
 
 				<!-- Locales dropdown -->
 				<Dropdown
-					:is-open="isLocaleDropdown"
-					class="bottom-0 left-0 right-0 z-20 translate-y-24 rounded-lg border-[1px] border-[#999999] bg-[#e1e8f2] p-2 transition-transform hover:scale-100 hover:text-black dark:bg-[#121212] dark:hover:text-white"
+					class="bottom-0 left-0 right-0 z-20 translate-y-24 rounded-lg border-[1px] border-[#747373] bg-[#e1e8f2] p-2 transition-transform hover:scale-100 hover:text-black dark:bg-[#121212] dark:hover:text-white"
 					:class="{
 						'scale-y-100': isLocaleDropdown,
 						'scale-y-0': !isLocaleDropdown,
