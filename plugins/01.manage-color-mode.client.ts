f388490c1c85d9bcd764d7585c944c983a10d766
chronosgit <warfacemeister@mail.ru>
2024-10-20 07:44:05 +0500

    ✨ feat: updated manage-color-mode client plugin

--- plugins/01.manage-color-mode.client.ts
+++ plugins/01.manage-color-mode.client.ts
@@ -1,27 +1,22 @@
 export default defineNuxtPlugin((nuxt) => {
-	const themeCookie = useCookie('theme');
+	const themeCookie = useCookie('theme', { path: '/', sameSite: 'strict' });
+
+	const isDarkMode = ref(themeCookie.value === 'dark');
 
 	const setLightMode = () => {
 		themeCookie.value = 'light';
-
+		isDarkMode.value = false;
 		document.documentElement.classList.remove('dark');
 	};
 
 	const setDarkMode = () => {
 		themeCookie.value = 'dark';
-
+		isDarkMode.value = true;
 		document.documentElement.classList.add('dark');
 	};
 
 	const toggleMode = () => {
-		const currentTheme = themeCookie.value;
-
-		if (!currentTheme) {
-			setDarkMode();
-			return;
-		}
-
-		if (currentTheme === 'dark') {
+		if (isDarkMode.value) {
 			setLightMode();
 		} else {
 			setDarkMode();
@@ -36,10 +31,21 @@ export default defineNuxtPlugin((nuxt) => {
 		}
 	};
 
-	nuxt.hooks.hook('app:beforeMount', () => respectOSPreference());
+	nuxt.hooks.hook('app:beforeMount', () => {
+		if (themeCookie.value) {
+			if (themeCookie.value === 'dark') {
+				setDarkMode();
+			} else {
+				setLightMode();
+			}
+		} else {
+			respectOSPreference();
+		}
+	});
 
 	return {
 		provide: {
+			isDarkMode,
 			setLightMode,
 			setDarkMode,
 			toggleMode,
