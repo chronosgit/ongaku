46d3d99da8f4e9424a1691ed19f19fd9e13fe0a2
chronosgit <warfacemeister@mail.ru>
2024-10-20 09:27:28 +0500

    🚧 wip: started working on MusicPlayer organism

--- components/organisms/MusicPlayer.vue
+++ components/organisms/MusicPlayer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="h-full w-full bg-gray-400">Player</div>
+	<div class="h-full max-h-20 dark:bg-black dark:text-white">Player</div>
 </template>
