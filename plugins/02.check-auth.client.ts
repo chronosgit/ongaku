41fce46fd36fb74633824e20586f3563d67b1212
chronosgit <warfacemeister@mail.ru>
2024-10-20 06:15:54 +0500

    🗑️ remove: removed check-auth client plugin

--- plugins/02.check-auth.client.ts
+++
@@ -1,9 +0,0 @@
-import { useCurrentUserStore } from '~/store/useCurrentUserStore';
-
-export default defineNuxtPlugin(async () => {
-	const curUserStore = useCurrentUserStore();
-
-	if (!curUserStore.isAuthenticated) return;
-
-	curUserStore.authenticate();
-});
