9d310f953d288c16b0a7873eaed46a88a68e3605
chronosgit <warfacemeister@mail.ru>
2024-10-20 05:09:30 +0500

    ✨ feat: updated ICurrentUser interface

--- interfaces/ICurrentUser.ts
+++ interfaces/ICurrentUser.ts
@@ -1,1 +1,15 @@
-export default interface ICurrentUser {}
+export default interface ICurrentUser {
+	id: string;
+	type: string;
+	email: string;
+	display_name: string;
+	country: string;
+	images: [];
+	followers: Object;
+
+	explicit_content: Object;
+	external_urls: Object;
+	product: string;
+	href: string;
+	uri: string;
+}
