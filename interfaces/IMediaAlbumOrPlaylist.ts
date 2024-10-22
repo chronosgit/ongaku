2f5413ac99b824cc0e9df18afa7dc4c7f5a23027
chronosgit <warfacemeister@mail.ru>
2024-10-22 19:51:03 +0500

    ✨ feat: added IMediaAlbumOrPlaylist interface

---
+++ interfaces/IMediaAlbumOrPlaylist.ts
@@ -0,0 +1,9 @@
+import type IImage from '~/interfaces/IImage';
+
+export default interface IMediaAlbumOrPlaylist {
+	id: string;
+	type: 'album' | 'playlist'; // album or playlist
+	name: string;
+	owner: string;
+	image: IImage;
+}
