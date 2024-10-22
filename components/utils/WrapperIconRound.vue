6b8ae761a07baa4db14de62f23805df170ff0dd8
chronosgit <warfacemeister@mail.ru>
2024-10-22 21:38:35 +0500

    ✨ feat: updated WrapperIconRound util component

--- components/utils/WrapperIconRound.vue
+++ components/utils/WrapperIconRound.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="flex items-center justify-center rounded-full bg-gray-200 p-2 text-gray-400 transition-all hover:scale-105 hover:text-black dark:bg-gray-800"
+		class="flex items-center justify-center rounded-full bg-gray-200 p-2 text-gray-400 transition-all hover:scale-105 hover:text-black dark:bg-gray-800 dark:hover:text-white"
 	>
 		<slot></slot>
 	</div>
