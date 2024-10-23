1c5ba2a9d8f5e53e671236a3dd4537e753e3dbf2
chronosgit <warfacemeister@mail.ru>
2024-10-23 23:08:29 +0500

    ✨ feat: updated SidebarLibrary layout component

---
+++ components/layout/sidebar-library/ItemContextMenu.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+	import { IconCheck, IconDelete, IconEdit } from '~/components/ui/icons';
+
+	const props = defineProps<{
+		type: 'playlist' | 'album';
+		isVisible: boolean;
+	}>();
+</script>
+
+<template>
+	<div
+		class="absolute z-10 flex min-w-14 flex-col gap-0.5 rounded-sm bg-zinc-200 p-1 shadow-lg dark:bg-zinc-800"
+		:class="{ block: props.isVisible, hidden: !props.isVisible }"
+	>
+		<!-- Playlist only feature -->
+		<template v-if="props.type === 'playlist'">
+			<div
+				class="group flex cursor-pointer items-center gap-2 px-2 py-1 transition-colors hover:bg-zinc-300 dark:hover:bg-zinc-900"
+			>
+				<IconDelete class="scale-125 text-zinc-600 dark:text-zinc-300" />
+
+				<p class="dark:text-white">
+					{{ $t('modules.sidebar-library.item-context-menu.delete-playlist') }}
+				</p>
+			</div>
+
+			<div
+				class="flex cursor-pointer items-center gap-2 px-2 py-1 transition-colors hover:bg-zinc-300 dark:hover:bg-zinc-900"
+			>
+				<IconEdit class="scale-125 text-zinc-600 dark:text-zinc-300" />
+
+				<p class="dark:text-white">
+					{{ $t('modules.sidebar-library.item-context-menu.edit-playlist') }}
+				</p>
+			</div>
+		</template>
+
+		<!-- Album only feature -->
+		<template v-else>
+			<div
+				class="group flex cursor-pointer items-center gap-2 px-2 py-1 transition-colors hover:bg-zinc-300 dark:hover:bg-zinc-900"
+			>
+				<div
+					class="flex items-center justify-center rounded-full bg-green-500 p-0.5"
+				>
+					<IconCheck class="text-white" />
+				</div>
+
+				<p class="dark:text-white">
+					{{ $t('modules.sidebar-library.item-context-menu.delete-album') }}
+				</p>
+			</div>
+		</template>
+	</div>
+</template>
--- components/layout/sidebar-library/closed/Items.vue
+++ components/layout/sidebar-library/closed/Items.vue
@@ -1,42 +1,12 @@
 <script setup lang="ts">
 	import type IMediaAlbumOrPlaylist from '~/interfaces/IMediaAlbumOrPlaylist';
-	import ItemTooltip from './ItemTooltip.vue';
-	import { IconQuestionMark } from '~/components/ui/icons';
+	import Item from './Item.vue';
 
 	const props = defineProps<{ items: IMediaAlbumOrPlaylist[] }>();
-
-	const onItemClick = (item: IMediaAlbumOrPlaylist) => {
-		console.log(item);
-	};
 </script>
 
 <template>
 	<div class="flex flex-col items-center gap-1">
-		<div
-			v-for="i in props.items"
-			class="group flex h-16 w-16 cursor-pointer items-center justify-center rounded-md transition-colors hover:bg-gray-200 dark:hover:bg-[#252525]"
-			@click="onItemClick(i)"
-		>
-			<NuxtImg
-				v-if="i.image.url"
-				:src="i.image.url"
-				class="h-4/5 w-4/5 rounded-md"
-			/>
-
-			<div
-				v-else
-				class="flex h-4/5 w-4/5 items-center justify-center rounded-md bg-zinc-300 dark:bg-zinc-700"
-			>
-				<IconQuestionMark class="scale-150 text-zinc-400 dark:text-zinc-500" />
-			</div>
-
-			<!-- Absolute tooltip to the right -->
-			<ItemTooltip
-				:name="i.name"
-				:type="i.type"
-				:owner="i.owner"
-				class="group-hover:block"
-			/>
-		</div>
+		<Item v-for="i in props.items" :item="i" />
 	</div>
 </template>
