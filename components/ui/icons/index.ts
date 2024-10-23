72cf7244b58fba493226a61a28c13064045b5d24
chronosgit <warfacemeister@mail.ru>
2024-10-23 23:08:13 +0500

    ✨ feat: added new ui components (icons)

--- components/ui/icons/index.ts
+++ components/ui/icons/index.ts
@@ -13,3 +13,6 @@ export { default as IconUser } from './User.vue';
 export { default as IconPlus } from './Plus.vue';
 export { default as IconClose } from './Close.vue';
 export { default as IconQuestionMark } from './QuestionMark.vue';
+export { default as IconEdit } from './Edit.vue';
+export { default as IconDelete } from './Delete.vue';
+export { default as IconCheck } from './Check.vue';
