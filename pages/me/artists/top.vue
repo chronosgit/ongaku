0a9492e9936c6962bda7e9b9501ff7f36cf49af9
chronosgit <warfacemeister@mail.ru>
2024-10-26 18:35:12 +0500

    🚧 wip: working on /me/artists/top page

--- pages/me/artists/top.vue
+++ pages/me/artists/top.vue
@@ -1,1 +1,14 @@
-<template>a</template>
+<script setup lang="ts">
+	import TemplateLayoutPart from '~/components/utils/TemplateLayoutPart.vue';
+
+	definePageMeta({
+		title: '/me/artists/top.meta.title',
+		layout: 'main',
+	});
+</script>
+
+<template>
+	<TemplateLayoutPart class="h-full overflow-hidden px-3">
+		HEllo
+	</TemplateLayoutPart>
+</template>
