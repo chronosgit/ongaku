e7836f45b6fd04792ab52ebcd16bbc26c613d923
chronosgit <warfacemeister@mail.ru>
2024-10-28 18:52:10 +0500

    ✨ feat: updated redirection middleware

--- middleware/01.redirect-not-found.global.ts
+++ middleware/01.redirect-not-found.global.ts
@@ -1,7 +1,7 @@
 export default defineNuxtRouteMiddleware(async (to) => {
 	const localePath = useLocalePath();
 
-	if (to.matched.length === 0 && to.path !== '/not-found') {
+	if (to.matched.length === 0 && to.path !== localePath('/not-found')) {
 		return navigateTo(localePath('/not-found'));
 	}
 });
