3c2f8a15cc9ca0dd8c8eb69d65b6982296a81bd5
chronosgit <warfacemeister@mail.ru>
2024-10-29 20:02:08 +0500

    🔥 feat: updated PlaylistsService

--- services/PlaylistsService.ts
+++ services/PlaylistsService.ts
@@ -1,7 +1,8 @@
 import type IServerApiSuccessResponse from '~/interfaces/IServerApiSuccessResponse';
+import type IPlaylistObject from '~/interfaces/business/playlists/IPlaylistObject';
 import type ISimplifiedPlaylistObject from '~/interfaces/business/playlists/ISimplifiedPlaylistObject';
 
-interface IFetchFollowedPlaylist extends IServerApiSuccessResponse {
+interface IFetchFollowedPlaylists extends IServerApiSuccessResponse {
 	data: {
 		href: string;
 		limit: number;
@@ -13,12 +14,41 @@ interface IFetchFollowedPlaylist extends IServerApiSuccessResponse {
 	};
 }
 
+interface ICreateNewPlaylistForUser extends IServerApiSuccessResponse {
+	data: IPlaylistObject;
+}
+
 export default class PlaylistsService {
 	static async fetchFollowedPlaylists(limit?: number, offset?: number) {
 		try {
-			const res = await $fetch<IFetchFollowedPlaylist>('/api/me/playlists', {
+			const res = await $fetch<IFetchFollowedPlaylists>('/api/me/playlists', {
 				params: { limit, offset },
 			});
+
+			return res;
+		} catch (err) {
+			throw err;
+		}
+	}
+
+	static async createNewPlaylistForUser(
+		userId: string,
+		namePrefix = 'Playlist_'
+	) {
+		try {
+			const {
+				data: { total },
+			} = await $fetch<IFetchFollowedPlaylists>('/api/me/playlists');
+
+			const name = `${namePrefix}${total}`;
+
+			const res = await $fetch<ICreateNewPlaylistForUser>(
+				`/api/users/${userId}/playlists`,
+				{
+					method: 'POST',
+					body: { name },
+				}
+			);
 
 			return res;
 		} catch (err) {
