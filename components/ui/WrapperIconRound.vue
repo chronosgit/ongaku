dcc5da63b050414b5aa000fce2fba5f025e0ce41
chronosgit <warfacemeister@mail.ru>
2024-10-29 19:07:21 +0500

    🎨 style: added .placeholder file

--- components/ui/WrapperIconRound.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div
-		class="flex items-center justify-center rounded-full bg-indigo-600 p-0.5 text-white transition-transform hover:scale-105"
-	>
-		<slot></slot>
-	</div>
-</template>
