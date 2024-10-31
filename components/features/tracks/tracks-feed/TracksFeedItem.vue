131939e3234167c9f3521a7316ea35a955ed2798
chronosgit <warfacemeister@mail.ru>
2024-10-31 15:51:53 +0500

    🚧 wip: working on TracksFeedItem context menu component

--- components/features/tracks/tracks-feed/TracksFeedItem.vue
+++ components/features/tracks/tracks-feed/TracksFeedItem.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
-	import { IconPlay, IconSettings } from '~/components/ui/icons';
+	import { IconPlay } from '~/components/ui/icons';
+	import TrackFeedItemContext from './TrackFeedItemContext.vue';
 	import type ITrackFeedItem from '~/interfaces/business/tracks/ITrackFeedItem';
 
 	const props = defineProps<{ item: ITrackFeedItem; order: number }>();
+
+	const { coords, isOpened, toggleCtxMenu } = useBaseContextMenu(
+		`tracks-feed-item-${props.item.id}`
+	);
 
 	const artistsNames = computed(() => {
 		const a = props.item.artists;
@@ -27,8 +32,14 @@
 
 <template>
 	<div
+		:ref="`tracks-feed-item-${props.item.id}`"
 		class="group/tracks flex items-center justify-between gap-1 overflow-x-hidden rounded-md p-2 text-sm font-medium transition-colors hover:bg-zinc-300 hover:bg-opacity-50 dark:text-white dark:hover:bg-zinc-800"
+		@contextmenu.prevent="toggleCtxMenu($event)"
 	>
+		<!-- Right-click absolute toggleable context menu -->
+		<TrackFeedItemContext :is-visible="isOpened" :coords="coords" />
+
+		<!-- Order number or play button -->
 		<div class="flex w-10 grow-0 items-center justify-center">
 			<p class="block group-hover/tracks:hidden">{{ props.order }}</p>
 
@@ -40,6 +51,7 @@
 			</ClientOnly>
 		</div>
 
+		<!-- Cover image, name and artists -->
 		<div
 			class="flex basis-[100%] items-center gap-3 overflow-x-hidden sm:basis-[60%] md:basis-[50%]"
 		>
@@ -60,6 +72,7 @@
 			</div>
 		</div>
 
+		<!-- Album name (w/ link) -->
 		<NuxtLink
 			class="hidden cursor-pointer overflow-hidden text-zinc-500 underline-offset-2 hover:underline sm:block sm:basis-[40%] md:basis-[30%] dark:text-zinc-400"
 			@click="console.log('Navigate to artist page'), props.item.album.name"
@@ -67,10 +80,12 @@
 			{{ props.item.album.name }}
 		</NuxtLink>
 
+		<!-- Added at -->
 		<p class="hidden text-zinc-500 md:block md:basis-[20%] dark:text-zinc-400">
 			{{ props.item.added_at }}
 		</p>
 
+		<!-- Duration -->
 		<p class="w-20 grow-0 text-center text-zinc-500 dark:text-zinc-400">
 			{{ formatDuration(props.item.duration_ms) }}
 		</p>
