99bb3e46d689658f32115bdf780d9a8feded3651
chronosgit <warfacemeister@mail.ru>
2024-11-11 20:49:04 +0500

    🚧 wip: working on player store adn aservice

---
+++ services/PlayerService.ts
@@ -0,0 +1,9 @@
+import type { FStartOrResumePlayback } from '~/interfaces/business/player/FGiveTrackToPlayer';
+
+class PlayerService {
+	static startOrResumePlayback(p: FStartOrResumePlayback) {
+		return;
+	}
+}
+
+export default PlayerService;
